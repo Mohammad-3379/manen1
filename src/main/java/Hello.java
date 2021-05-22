4b2aee3969dca971af166d3436774d3fc3526974
Mohammad K <alkhedrmohammad@yahoo.com>
2021-05-22 07:10:04 -0500

    started hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
+
     }
 }
